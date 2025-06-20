6c5d49e8b0c7e670cae67292b1dfab594141a46a
Muhammad-Junaid-586 <154722812+Muhammad-Junaid-586@users.noreply.github.com>
2025-06-20 11:48:44 +0500

    remove the bug

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { cn } from "@/lib/utils";
+
 import { Spotlight } from "./ui/Spotlight";
 import { Button } from "./ui/moving-border";
 
